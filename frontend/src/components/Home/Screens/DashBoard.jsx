6cc05965839768a427c096c5bdfc358deefe86f9
aarfazafar <aarfazafar2@gmail.com>
2024-10-04 23:59:00 +0530

    carousel capsules

--- frontend/src/components/Home/Screens/DashBoard.jsx
+++ frontend/src/components/Home/Screens/DashBoard.jsx
@@ -56,75 +56,105 @@ const DashBoard = ({ menuClicked, setMenuClicked }) => {
             data-bs-ride="carousel"
           >
             <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="4000" style={{height:"24vh", background:"#f8f9fa"}}>
+              <div
+                class="carousel-item active"
+                data-bs-interval="4000"
+                style={{ height: "24vh", background: "#f8f9fa" }}
+              >
+                <div className="row mt-2 text-center">
+                  <div className="col-md-4">
+                    <div className="capsule border">
+                      <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrz8f66U-IU8hFujqUeqB06jbkiZ1uVHu8dQ&s"
+                        alt=""
+                        className="capsule-img"
+                      />
+                      <h4 className="foot-title">Veggies Delight</h4>
+                      <p>Fresh Veggies</p>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+
+                  <div className="capsule border"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrz8f66U-IU8hFujqUeqB06jbkiZ1uVHu8dQ&s" alt="" className="capsule-img"/>
+                      <h4  className="foot-title">Non veg</h4>
+                      <p>
+                      For meat Lovers
+                    </p>
+                      </div>
+
+
+                  </div>
+                  <div className="col-md-4">
+                  <div className="capsule border"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrz8f66U-IU8hFujqUeqB06jbkiZ1uVHu8dQ&s" alt="" className="capsule-img"/>
+                      <h4  className="foot-title">Mushroom Magic</h4>
+                      <p>
+                      Lorem ipsum 
+                    </p>
+                      </div>
+
+                  </div>
+                </div>
+              </div>
+              <div
+                class="carousel-item"
+                data-bs-interval="4000"
+                style={{ height: "24vh", background: "#f8f9fa" }}
+              >
                 <div className="row mt-5 text-center">
                   <div className="col-md-4 d-flex">
                     <div className="d-flex flex-column">
                       <h4 className="foot-title">Customizable</h4>
                       <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam vitae nostrum dolorum voluptate, error consectetur!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Laboriosam vitae nostrum dolorum voluptate, error
+                        consectetur!
                       </p>
                     </div>
                   </div>
                   <div className="col-md-4">
                     <h4 className="foot-title">Fresh</h4>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam exercitationem earum, harum dolor hic amet!
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Quam exercitationem earum, harum dolor hic amet!
                     </p>
                   </div>
                   <div className="col-md-4">
                     <h4 className="foot-title">Fast Delivery</h4>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat tempore corporis rem.
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Fugiat tempore corporis rem.
                     </p>
                   </div>
                 </div>
               </div>
-              <div class="carousel-item" data-bs-interval="4000" style={{height:"24vh", background:"#f8f9fa"}}>
-              <div className="row mt-5 text-center">
+              <div
+                class="carousel-item"
+                data-bs-interval="4000"
+                style={{ height: "24vh", background: "#f8f9fa" }}
+              >
+                <div className="row mt-5 text-center">
                   <div className="col-md-4 d-flex">
                     <div className="d-flex flex-column">
                       <h4 className="foot-title">Customizable</h4>
                       <p>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam vitae nostrum dolorum voluptate, error consectetur!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Laboriosam vitae nostrum dolorum voluptate, error
+                        consectetur!
                       </p>
                     </div>
                   </div>
                   <div className="col-md-4">
                     <h4 className="foot-title">Fresh</h4>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam exercitationem earum, harum dolor hic amet!
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Quam exercitationem earum, harum dolor hic amet!
                     </p>
                   </div>
                   <div className="col-md-4">
                     <h4 className="foot-title">Fast Delivery</h4>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat tempore corporis rem.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item" data-bs-interval="4000" style={{height:"24vh", background:"#f8f9fa"}}>
-              <div className="row mt-5 text-center">
-                  <div className="col-md-4 d-flex">
-                    <div className="d-flex flex-column">
-                      <h4 className="foot-title">Customizable</h4>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam vitae nostrum dolorum voluptate, error consectetur!
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-4">
-                    <h4 className="foot-title">Fresh</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam exercitationem earum, harum dolor hic amet!
-                    </p>
-                  </div>
-                  <div className="col-md-4">
-                    <h4 className="foot-title">Fast Delivery</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat tempore corporis rem.
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Fugiat tempore corporis rem.
                     </p>
                   </div>
                 </div>
@@ -155,7 +185,7 @@ const DashBoard = ({ menuClicked, setMenuClicked }) => {
               <span class="visually-hidden">Next</span>
             </button>
           </div>
-          <hr  ref={ServicesRef} />
+          <hr ref={ServicesRef} style={{ overflow: "auto" }} />
         </div>
       </div>
     </div>
