9ee1450bb1c5f93ca3dc17d1a947668a5953119b
aarfazafar <aarfazafar2@gmail.com>
2025-03-05 18:56:56 +0530

    cors issue fixed

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,38 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config(); 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectToDatabase = require("./models/db");
 connectToDatabase();
-const dotenv = require('dotenv')
-const FRONTEND = process.env.VITE_FRONTEND_URL;
 
+const FRONTEND = process.env.VITE_FRONTEND_URL || "http://localhost:5173";
 
 const corsOptions = {
   origin: [FRONTEND, "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-dotenv.config()
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-app.use(express.json());
 app.use("/api", require("./Routes/createUser"));
 app.use("/api", require("./Routes/DisplayData"));
-
 app.use("/api", require("./Routes/userLogin"));
 app.use("/api", require("./Routes/OrderData"));
 app.use("/api", require("./Routes/MyOrderData"));
 app.use("/api", require("./Routes/DeleteOrder"));
 
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-
-
-// app.use((req, res, next)=> {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next();
-// })
