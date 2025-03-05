4cf29e315a7025d11ee2bf8b619ce88bbc9d6b9c
aarfazafar <aarfazafar2@gmail.com>
2025-03-05 19:18:03 +0530

    cors issue fixed

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,16 @@ app.use(
   cors({
     origin:[FRONTEND, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 
